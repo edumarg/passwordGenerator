337f7c196eb35e8b9e07d81ec81a980773d71d80
Eduardo Margulis <edumarg@gmx.com>
2021-05-03 10:48:56 +0300

    added basic logic to generate passwords

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import random
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+-=/?.,"
+
+
 def generate_passwords(ammount, length):
-    print(ammount, length)
+    passwords = []
+    while ammount > 0:
+        password = ""
+        number = length
+        while number > 0:
+            char = random.choice(chars)
+            password = password + char
+            number -= 1
+        passwords.append(password)
+        ammount -= 1
+    return passwords
 
 
 if __name__ == '__main__':
@@ -7,4 +22,6 @@ if __name__ == '__main__':
     print("==================")
     number_of_passwords = int(input("Number of password you will like to generate? \n>>"))
     password_length = int(input("Password length?\n>>"))
-    generate_passwords(number_of_passwords, password_length)
+    passwords = generate_passwords(number_of_passwords, password_length)
+    for password in passwords:
+        print(password)
