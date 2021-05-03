55e4acdee57bd14da1f03b92bd74bd5ffde65b8c
Eduardo Margulis <edumarg@gmx.com>
2021-05-03 10:25:21 +0300

    created code template

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+def generate_passwords(ammount, length):
+    print(ammount, length)
+
 
 if __name__ == '__main__':
-
+    print("Password Generator")
+    print("==================")
+    number_of_passwords = int(input("Number of password you will like to generate? \n>>"))
+    password_length = int(input("Password length?\n>>"))
+    generate_passwords(number_of_passwords, password_length)
